1544550ed0ef14fa9352eb354ec9c6380884d79b
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-10-11 18:46:37 -0300

    fix header: mejora en el js del menú mobile.

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -15,35 +15,40 @@ export class HeaderComponent {
   }
 
   private initializeEventHandlers() {
-    const mobileNavToggle =
-      this.elementRef.nativeElement.querySelector('.mobile-nav-toggle');
+    const mobileNavToggle = this.elementRef.nativeElement.querySelector('.mobile-nav-toggle');
     const navbar = this.elementRef.nativeElement.querySelector('#navbar');
-
+    const navLinks = this.elementRef.nativeElement.querySelectorAll('.navbar ul li a');
+  
     if (mobileNavToggle && navbar) {
       mobileNavToggle.addEventListener('click', () => {
         navbar.classList.toggle('navbar-mobile');
         mobileNavToggle.classList.toggle('bi-list');
         mobileNavToggle.classList.toggle('bi-x');
       });
-
-      const dropdownLinks = this.elementRef.nativeElement.querySelectorAll(
-        '.navbar .dropdown > a'
-      );
+  
+      navLinks.forEach((link: HTMLElement) => {
+        link.addEventListener('click', () => {
+          if (navbar.classList.contains('navbar-mobile')) {
+            navbar.classList.remove('navbar-mobile');
+            mobileNavToggle.classList.toggle('bi-list');
+            mobileNavToggle.classList.toggle('bi-x');
+          }
+        });
+      });
+  
+      const dropdownLinks = this.elementRef.nativeElement.querySelectorAll('.navbar .dropdown > a');
       dropdownLinks.forEach((link: HTMLElement) => {
-        link.addEventListener(
-          'click',
-          (e: Event) => {
-            if (navbar.classList.contains('navbar-mobile')) {
-              e.preventDefault();
-              const dropdownMenu = link.nextElementSibling as HTMLElement;
-              dropdownMenu.classList.toggle('dropdown-active');
-            }
-          },
-          true
-        );
+        link.addEventListener('click', (e: Event) => {
+          if (navbar.classList.contains('navbar-mobile')) {
+            e.preventDefault();
+            const dropdownMenu = link.nextElementSibling as HTMLElement;
+            dropdownMenu.classList.toggle('dropdown-active');
+          }
+        }, true);
       });
     }
   }
+  
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
