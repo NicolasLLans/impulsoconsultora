fd313c0e143228a5db8a6f144f099ec5d0be55bf
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-10-11 22:27:39 -0300

    fix footer: mejora en los enlaces utiles

--- src/app/componentes/z-footer/z-footer.component.ts
+++ src/app/componentes/z-footer/z-footer.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-z-footer',
   templateUrl: './z-footer.component.html',
-  styleUrls: ['./z-footer.component.css']
+  styleUrls: ['./z-footer.component.css'],
 })
 export class ZFooterComponent {
-
+  scrollTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
