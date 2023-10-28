6b5c82695ba6085137a8a4d289640fc99da0ba1d
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-10-28 20:46:37 -0300

    fix quienes somos:(diseño y submenu)

--- src/app/quienes-somos/quienes-somos/quienes-somos.component.ts
+++ src/app/quienes-somos/quienes-somos/quienes-somos.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class QuienesSomosComponent {
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const culturaLink = document.getElementById('cultura-link');
+    const valoresLink = document.getElementById('valores-link');
+    const rolLink = document.getElementById('rol-link');
+    if (culturaLink) {
+      culturaLink.addEventListener('click', () => {
+        const culturaSection = document.getElementById('cultura');
+        if (culturaSection) {
+          culturaSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    }
+    if (valoresLink) {
+      valoresLink.addEventListener('click', () => {
+        const valoresSection = document.getElementById('valores');
+        if (valoresSection) {
+          valoresSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    }
+    if (rolLink) {
+      rolLink.addEventListener('click', () => {
+        const rolSection = document.getElementById('rol');
+        if (rolSection) {
+          rolSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    }
+  }
 }
