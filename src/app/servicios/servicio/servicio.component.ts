bf282ea5a52e0f1f506cc20aa28b43bee7ab5d19
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-10-29 19:03:13 -0300

    fix links-para servicios:(anchors y sections)

--- src/app/servicios/servicio/servicio.component.ts
+++ src/app/servicios/servicio/servicio.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servicio',
   templateUrl: './servicio.component.html',
-  styleUrls: ['./servicio.component.css']
+  styleUrls: ['./servicio.component.css'],
 })
 export class ServicioComponent {
+  constructor(private route: ActivatedRoute, private router: Router) {
+    // Detectar cambios de ruta para realizar el scroll
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const fragment = this.route.snapshot.fragment;
+        if (fragment) {
+          const element = document.getElementById(fragment);
+          if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+          }
+        }
+      }
+    });
+  }
 
+  ngOnInit(): void {}
 }
